e4f62e3c4b4634659802e82d94cac8affe356d54
Kinddle <taniamalikSSS@gmail.com>
2016-03-14 17:57:51 +0200

    home page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () {
+// 	// global vars
+// 	var winWidth = $(window).width();
+// 	var winHeight = $(window).height();
+
+// 	// set initial div height / width
+// 	$('.front-header').css({
+//         'width': winWidth,
+//         'height': winHeight,
+// 	});
+
+// 	// make sure div stays full width/height on resize
+// 	$(window).resize(function(){
+// 		console.log('resized');
+//         $('.front-header').css({
+//         'width': winWidth,
+//         'height': winHeight,
+//         });
+//     });
+// });
